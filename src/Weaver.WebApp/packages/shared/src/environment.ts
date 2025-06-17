5d285f73cca80d84b516474cac205afd784ab422
ru1t3rl <lucaruiters@hotmail.nl>
2025-06-17 20:57:11 +0200

    Update environment.ts, remove commented code

--- src/Weaver.WebApp/packages/shared/src/environment.ts
+++ src/Weaver.WebApp/packages/shared/src/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    apiAddress: import.meta.env.services__webapi__http__0 ?? import.meta.env.VITE_API_ADDRESS
-    // apiAddress: 'http://localhost:40203'   
-}
+  apiAddress:
+    import.meta.env.services__webapi__http__0 ??
+    import.meta.env.VITE_API_ADDRESS,
+};
