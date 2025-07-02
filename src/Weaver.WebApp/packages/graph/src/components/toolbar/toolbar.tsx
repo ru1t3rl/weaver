83949441cb96e21246c4c280b43da0690308b826
ru1t3rl <lucaruiters@hotmail.nl>
2025-07-02 21:56:01 +0200

    Add a simple toolbar

--- src/Weaver.WebApp/packages/graph/src/components/toolbar/toolbar.tsx
+++ src/Weaver.WebApp/packages/graph/src/components/toolbar/toolbar.tsx
@@ -1,11 +1,20 @@
 import {Button, Flex} from "antd";
-import {LuPlus} from "react-icons/lu";
+import {LuFolderOpen, LuPlus, LuSave} from "react-icons/lu";
 import styles from './toolbar.module.scss';
+import {useServiceSearchModal} from "../../hooks/use-service-search-modal";
 
 export function Toolbar() {
+    const {show: showModal} = useServiceSearchModal();
+
+    function handleAddNewClicked() {
+        showModal();
+    }
+
     return (
-        <Flex className={styles['toolbar-container']}>
-            <Button shape='round' icon={<LuPlus/>} onClick={() => {}}/>
+        <Flex className={styles['toolbar-container']} gap={10}>
+            <Button shape='round' icon={<LuFolderOpen/>}/>
+            <Button shape='round' icon={<LuSave/>}/>
+            <Button shape='round' icon={<LuPlus/>} onClick={handleAddNewClicked}/>
         </Flex>
     )
 }
