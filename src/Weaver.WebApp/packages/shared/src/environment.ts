7581c21ebaefe9d3ab930771454669933905f959
ru1t3rl <lucaruiters@hotmail.nl>
2025-07-05 21:34:00 +0200

    Fix reading environment variable for aspire webapi

--- src/Weaver.WebApp/packages/shared/src/environment.ts
+++ src/Weaver.WebApp/packages/shared/src/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    apiAddress: import.meta.env.services__webapi__http__0 ?? import.meta.env.VITE_API_ADDRESS
-    // apiAddress: 'http://localhost:40203'   
+    apiAddress: process.env.services__webapi__http__0 ?? import.meta.env.VITE_API_ADDRESS
 }
