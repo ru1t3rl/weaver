07c07cc2c158e56f550bc25fc3cdaa707f2991f6
ru1t3rl <lucaruiters@hotmail.nl>
2025-07-06 15:33:39 +0200

    Add on selection change event

--- src/Weaver.WebApp/packages/graph/src/events/node-events.ts
+++ src/Weaver.WebApp/packages/graph/src/events/node-events.ts
@@ -6,6 +6,7 @@ enum NodeEvents {
   Update = 'update-node',
   Remove = 'remove-node',
   AllUpdates = 'all-node-updates',
+  Selection = 'select-node',
 }
 
 export interface INodeEvents<TNode extends Node> {
@@ -13,6 +14,7 @@ export interface INodeEvents<TNode extends Node> {
   onUpdateNode: (node: TNode) => void;
   onRemoveNode: (node: TNode) => void;
   onAllNodeUpdates: (nodes: TNode[]) => void;
+  onSelectionChanged: (node: TNode | undefined) => void;
 }
 
 export function useNodeEvents<TNode extends Node>(): INodeEvents<TNode> {
@@ -38,11 +40,19 @@ export function useNodeEvents<TNode extends Node>(): INodeEvents<TNode> {
     window.dispatchEvent(event);
   }
 
+  function onSelectionChanged(node: TNode | undefined) {
+    const event = new CustomEvent<TNode | undefined>(NodeEvents.Selection, {
+      detail: node,
+    });
+    window.dispatchEvent(event);
+  }
+
   return {
     onAddNode,
     onUpdateNode,
     onRemoveNode,
     onAllNodeUpdates,
+    onSelectionChanged,
   };
 }
 
@@ -51,12 +61,11 @@ interface UseNodeEventListener<TNode extends Node> {
   onUpdateNode?: (node: TNode) => void;
   onRemoveNode?: (node: TNode) => void;
   onAllNodeUpdates?: (nodes: TNode[]) => void;
+  onSelectionChanged?: (node: TNode | undefined) => void;
 }
 
-export function useNodeEventListener<TNode extends Node>(
-  callbacks: UseNodeEventListener<TNode>,
-) {
-  const { onAddNode, onUpdateNode, onRemoveNode, onAllNodeUpdates } = callbacks;
+export function useNodeEventListener<TNode extends Node>(callbacks: UseNodeEventListener<TNode>) {
+  const { onAddNode, onUpdateNode, onRemoveNode, onAllNodeUpdates, onSelectionChanged } = callbacks;
 
   const handleOnAddNode = useCallback(
     (e: Event) => {
@@ -98,17 +107,29 @@ export function useNodeEventListener<TNode extends Node>(
     [onAllNodeUpdates],
   );
 
+  const handleOnSelectionChanged = useCallback(
+    (e: Event) => {
+      if (onSelectionChanged) {
+        const { detail } = e as CustomEvent<TNode | undefined>;
+        onSelectionChanged(detail);
+      }
+    },
+    [onSelectionChanged],
+  );
+
   useMemo(() => {
     window.addEventListener(NodeEvents.Add, handleOnAddNode);
     window.addEventListener(NodeEvents.Remove, handleOnRemoveNode);
     window.addEventListener(NodeEvents.Update, handleOnUpdateNode);
     window.addEventListener(NodeEvents.AllUpdates, handleOnAllNodeUpdates);
+    window.addEventListener(NodeEvents.Selection, handleOnSelectionChanged);
 
     return () => {
       window.removeEventListener(NodeEvents.Add, handleOnAddNode);
       window.removeEventListener(NodeEvents.Remove, handleOnRemoveNode);
       window.removeEventListener(NodeEvents.Update, handleOnUpdateNode);
       window.removeEventListener(NodeEvents.AllUpdates, handleOnAllNodeUpdates);
+      window.removeEventListener(NodeEvents.Selection, handleOnSelectionChanged);
     };
-  }, []);
+  }, [handleOnAddNode, handleOnAllNodeUpdates, handleOnRemoveNode, handleOnSelectionChanged, handleOnUpdateNode]);
 }
