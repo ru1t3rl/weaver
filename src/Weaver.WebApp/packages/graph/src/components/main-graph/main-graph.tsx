b2e8825b0c9c790be47df93c8fc473d6e165add1
ru1t3rl <lucaruiters@hotmail.nl>
2025-07-09 20:44:39 +0200

    Enable shift-/multiselection

--- src/Weaver.WebApp/packages/graph/src/components/main-graph/main-graph.tsx
+++ src/Weaver.WebApp/packages/graph/src/components/main-graph/main-graph.tsx
@@ -1,62 +1,82 @@
-import { Background, Node, NodeChange, ReactFlowProvider, useNodesState } from '@xyflow/react';
+import {Background, Node, NodeChange, ReactFlowProvider, useNodesState} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { Flex } from 'antd';
-import { useCallback, useRef } from 'react';
-import { useNodeEventListener } from '../../events';
-import { useServiceGraph } from '../../hooks/use-service-graph';
-import { ServiceSearchModalProvider } from '../../providers/service-search-modal-provider';
-import { NodeInfoPanel } from '../node-info-panel/node-info-panel';
+import {Flex} from 'antd';
+import {useCallback, useRef} from 'react';
+import {useNodeEventListener} from '../../events';
+import {useServiceGraph} from '../../hooks/use-service-graph';
+import {ServiceSearchModalProvider} from '../../providers/service-search-modal-provider';
+import {NodeInfoPanel} from '../node-info-panel/node-info-panel';
 import ServiceNode from '../nodes/service-node/service-node';
 import StyledGraph from '../styled/styled-graph';
-import { Toolbar } from '../toolbar/toolbar';
+import {Toolbar} from '../toolbar/toolbar';
 import styles from './main-graph.module.scss';
 
-interface MainGraphProps {}
+interface MainGraphProps {
+}
 
 const nodeTypes = {
-  serviceNode: ServiceNode,
+    serviceNode: ServiceNode,
 };
 
 export function MainGraph(props: MainGraphProps) {
-  const [nodes, _setNodes, onNodesChange] = useNodesState([] as Node[]);
-  const { tryUpdateNodes } = useServiceGraph();
+    const [nodes, _setNodes, onNodesChange] = useNodesState([] as Node[]);
+    const {tryUpdateNodes} = useServiceGraph();
 
-  const setNodes = useRef((nodes: Node[]) => {
-    _setNodes(nodes);
-  });
+    const setNodes = useRef((nodes: Node[]) => {
+        _setNodes(nodes);
+    });
 
-  useNodeEventListener({
-    onAllNodeUpdates: value => setNodes.current(value),
-  });
+    useNodeEventListener({
+        onAllNodeUpdates: value => setNodes.current(value),
+    });
 
-  const customOnNodesChange = useCallback(
-    (changes: NodeChange<Node>[]) => {
-      tryUpdateNodes(changes as NodeChange<ServiceNode>[]);
-      onNodesChange(changes);
-    },
-    [onNodesChange, tryUpdateNodes],
-  );
+    const customOnNodesChange = useCallback(
+        (changes: NodeChange<Node>[]) => {
+            tryUpdateNodes(changes as NodeChange<ServiceNode>[]);
+            onNodesChange(changes);
+        },
+        [onNodesChange, tryUpdateNodes],
+    );
 
-  return (
-    <div style={{ width: '100%', height: '100%' }} className={styles['main-graph-container']}>
-      <ReactFlowProvider>
-        <ServiceSearchModalProvider
-          keybinding={{
-            key: ' ',
-            ctrl: true,
-          }}
-        >
-          <Flex vertical className={styles['overlay-ui']}>
-            <Toolbar />
-            <NodeInfoPanel />
-          </Flex>
-          <StyledGraph nodes={nodes} edges={[]} nodeTypes={nodeTypes} snapToGrid onNodesChange={customOnNodesChange}>
-            <Background />
-          </StyledGraph>
-        </ServiceSearchModalProvider>
-      </ReactFlowProvider>
-    </div>
-  );
+    const customOnNodeSelectionChange = useCallback(({nodes,}: { nodes: Node[] }) => {
+        const changes: NodeChange<ServiceNode>[] = nodes.map(node => ({
+            id: node.id,
+            type: 'select',
+            selected: node.selected,
+        }) as NodeChange<ServiceNode>);
+
+        tryUpdateNodes(changes);
+        onNodesChange(changes);
+    }, [onNodesChange, tryUpdateNodes]);
+
+    return (
+        <div style={{width: '100%', height: '100%'}} className={styles['main-graph-container']}>
+            <ReactFlowProvider>
+                <ServiceSearchModalProvider
+                    keybinding={{
+                        key: ' ',
+                        ctrl: true,
+                    }}
+                >
+                    <Flex vertical className={styles['overlay-ui']}>
+                        <Toolbar/>
+                        <NodeInfoPanel/>
+                    </Flex>
+                    <StyledGraph
+                        nodes={nodes}
+                        edges={[]}
+                        nodeTypes={nodeTypes}
+                        snapToGrid
+                        onNodesChange={customOnNodesChange}
+                        onSelectionChange={customOnNodeSelectionChange}
+                        multiSelectionKeyCode={'Ctrl'}
+                        selectionKeyCode={'Shift'}>
+                        <Background/>
+                    </StyledGraph>
+                </ServiceSearchModalProvider>
+            </ReactFlowProvider>
+        </div>
+    );
 }
 
 export default MainGraph;
--- src/Weaver.WebApp/packages/graph/src/providers/service-graph-provider.tsx
+++ src/Weaver.WebApp/packages/graph/src/providers/service-graph-provider.tsx
@@ -1,107 +1,101 @@
-import { NodeChange } from '@xyflow/react';
-import { PropsWithChildren, useRef } from 'react';
-import { v4 as uuid } from 'uuid';
-import { ServiceNode, nodeName as serviceNodeName } from '../components/nodes/service-node/service-node';
-import { createServiceGraphContext, IServiceGraphContext } from '../contexts';
-import { useNodeEvents } from '../events';
+import {NodeChange} from '@xyflow/react';
+import {PropsWithChildren, useRef} from 'react';
+import {v4 as uuid} from 'uuid';
+import {ServiceNode, nodeName as serviceNodeName} from '../components/nodes/service-node/service-node';
+import {createServiceGraphContext, IServiceGraphContext} from '../contexts';
+import {useNodeEvents} from '../events';
 
 export const ServiceGraphContext = createServiceGraphContext<ServiceNode>();
-export function ServiceGraphProvider({ children }: PropsWithChildren) {
-  const nodes = useRef<Map<string, ServiceNode>>(new Map());
 
-  const defaultNode: ServiceNode = {
-    deletable: true,
-    draggable: true,
-    dragging: false,
-    selectable: true,
-    selected: false,
-    zIndex: 0,
-    connectable: true,
-    type: serviceNodeName,
-    id: uuid(),
-    position: {
-      x: 0,
-      y: 0,
-    },
-    data: {
-      serviceInfo: {
-        name: 'Unknown',
-        type: 'Reference',
-      },
-    },
-  };
+export function ServiceGraphProvider({children}: PropsWithChildren) {
+    const nodes = useRef<Map<string, ServiceNode>>(new Map());
 
-  const events = useNodeEvents();
-
-  function tryAddNode(node: Partial<ServiceNode>): boolean {
-    const newNode: ServiceNode = {
-      ...defaultNode,
-      id: uuid(),
-      ...node,
+    const defaultNode: ServiceNode = {
+        deletable: true,
+        draggable: true,
+        dragging: false,
+        selectable: true,
+        selected: false,
+        zIndex: 0,
+        connectable: true,
+        type: serviceNodeName,
+        id: uuid(),
+        position: {
+            x: 0,
+            y: 0,
+        },
+        data: {
+            serviceInfo: {
+                name: 'Unknown',
+                type: 'Reference',
+            },
+        },
     };
 
-    if (nodes.current.has(newNode.id)) {
-      return false;
-    }
+    const events = useNodeEvents();
 
-    nodes.current.set(newNode.id, newNode);
-    events.onAddNode(newNode);
-    events.onAllNodeUpdates(Array.from(nodes.current.values()));
-    return true;
-  }
+    function tryAddNode(node: Partial<ServiceNode>): boolean {
+        const newNode: ServiceNode = {
+            ...defaultNode,
+            id: uuid(),
+            ...node,
+        };
 
-  function tryRemoveNode(node: ServiceNode): boolean {
-    if (!nodes.current.has(node.id)) {
-      return false;
-    }
-
-    nodes.current.delete(node.id);
-    events.onRemoveNode(node);
-    events.onAllNodeUpdates(Array.from(nodes.current.values()));
-    return true;
-  }
-
-  function tryUpdateNodes(changes: NodeChange<ServiceNode>[]): boolean {
-    for (const change of changes) {
-      let node: ServiceNode | undefined;
-      if (change.type === 'select' && (node = nodes.current.get(change.id))) {
-        nodes.current.forEach(n => {
-          if (n.selected) {
-            events.onUpdateNode(n);
-            n.selected = false;
-
-            events.onSelectionChanged(undefined);
-          }
-        });
-
-        if (node.selected !== change.selected) {
-          node.selected = change.selected;
-          nodes.current.set(node.id, node);
-          events.onUpdateNode(node);
-
-          events.onSelectionChanged(node.selected ? node : undefined);
+        if (nodes.current.has(newNode.id)) {
+            return false;
         }
-      }
 
-      if (change.type === 'position' && (node = nodes.current.get(change.id))) {
-        node.dragging = change.dragging ?? false;
-        node.position = change.position ?? node.position;
-
-        nodes.current.set(node.id, node);
-        events.onUpdateNode(node);
-      }
+        nodes.current.set(newNode.id, newNode);
+        events.onAddNode(newNode);
+        events.onAllNodeUpdates(Array.from(nodes.current.values()));
+        return true;
     }
 
-    events.onAllNodeUpdates(Array.from(nodes.current.values()));
-    return true;
-  }
+    function tryRemoveNode(node: ServiceNode): boolean {
+        if (!nodes.current.has(node.id)) {
+            return false;
+        }
 
-  const contextValue: IServiceGraphContext<ServiceNode> = {
-    tryAddNode,
-    tryRemoveNode,
-    tryUpdateNodes,
-    nodes: Array.from(nodes.current.values()),
-  };
+        nodes.current.delete(node.id);
+        events.onRemoveNode(node);
+        events.onAllNodeUpdates(Array.from(nodes.current.values()));
+        return true;
+    }
 
-  return <ServiceGraphContext.Provider value={contextValue}>{children}</ServiceGraphContext.Provider>;
+    function tryUpdateNodes(changes: NodeChange<ServiceNode>[]): boolean {
+        const amountSelected = changes.filter(change => change.type === 'select').length;
+
+        for (const change of changes) {
+            let node: ServiceNode | undefined;
+            if (change.type === 'select' && (node = nodes.current.get(change.id))) {
+                if (node.selected !== change.selected) {
+                    node.selected = change.selected;
+                    nodes.current.set(node.id, node);
+                    events.onUpdateNode(node);
+
+                    events.onSelectionChanged(node.selected && amountSelected == 1 ? node : undefined);
+                }
+            }
+
+            if (change.type === 'position' && (node = nodes.current.get(change.id))) {
+                node.dragging = change.dragging ?? false;
+                node.position = change.position ?? node.position;
+
+                nodes.current.set(node.id, node);
+                events.onUpdateNode(node);
+            }
+        }
+
+        events.onAllNodeUpdates(Array.from(nodes.current.values()));
+        return true;
+    }
+
+    const contextValue: IServiceGraphContext<ServiceNode> = {
+        tryAddNode,
+        tryRemoveNode,
+        tryUpdateNodes,
+        nodes: Array.from(nodes.current.values()),
+    };
+
+    return <ServiceGraphContext.Provider value={contextValue}>{children}</ServiceGraphContext.Provider>;
 }
