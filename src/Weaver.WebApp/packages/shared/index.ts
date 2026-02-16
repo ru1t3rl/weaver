cec1bc3a92c3187ccfb8e08fe8dd399f446a4ce8
ru1t3rl <lucaruiters@hotmail.nl>
2026-02-16 22:38:01 +0100

    Ad missing export

--- src/Weaver.WebApp/packages/shared/index.ts
+++ src/Weaver.WebApp/packages/shared/index.ts
@@ -1,5 +1,6 @@
-export * from './src/environment';
 export * from './src/api';
 export * from './src/axios-request-configs';
 export * from './src/components/layout/layout';
+export * from './src/environment';
+export * from './src/routes';
 export * from './src/weaver-logo';
