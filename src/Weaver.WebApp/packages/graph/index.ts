709672c35ed20093ff41e4178de52d9e2f21555b
ru1t3rl <lucaruiters@hotmail.nl>
2026-03-14 16:37:23 +0100

    Add missing export

--- src/Weaver.WebApp/packages/graph/index.ts
+++ src/Weaver.WebApp/packages/graph/index.ts
@@ -3,6 +3,7 @@ export * from './src/providers/graph-provider';
 export * from './src/providers/graph-provider-ref';
 export * from './src/hooks/use-graph';
 export * from './src/hooks/use-graph-ref';
+export * from './src/hooks/use-inspector';
 
 export { ContextMenuProvider } from './src/providers/context-menu-provider';
 export * from './src/hooks/use-context-menu';
