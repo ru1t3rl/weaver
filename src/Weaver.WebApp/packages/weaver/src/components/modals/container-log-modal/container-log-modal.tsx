ce03ae841b12291a227a430808c79406101106ad
ru1t3rl <lucaruiters@hotmail.nl>
2026-03-21 13:33:44 +0100

    Add a simple backdrop

--- src/Weaver.WebApp/packages/weaver/src/components/modals/container-log-modal/container-log-modal.tsx
+++ src/Weaver.WebApp/packages/weaver/src/components/modals/container-log-modal/container-log-modal.tsx
@@ -4,6 +4,7 @@ import { CodeBlock } from "@weaver/shared";
 import { Card, Flex, Input, Spin, Tooltip, Typography } from "antd";
 import { ChangeEvent, useState } from "react";
 import styles from './container-log-modal.module.scss';
+import { Backdrop } from "../backdrop/backdrop";
 
 export const ContainerLogModal = () => {
     const containerId = '8803a75db1a5936f06efbc152d1b8e0b55d26e4c0cac2cef4510815b5121f75f';
@@ -30,32 +31,34 @@ export const ContainerLogModal = () => {
     }
 
     return (
-        <ClickAwayListener onClickAway={() => { }}>
-            <Card className={styles['modal-container']}>
-                <Flex vertical gap={10}>
-                    {dataIsLoading && <Spin />}
-                    {!dataIsLoading && <CodeBlock
-                        title={data?.names[0] ?? 'Log'}
-                        content={lines.join('\n')}
-                        tools={[
-                            <Flex align={'center'} gap={5}>
-                                <Typography.Paragraph style={{ marginBottom: 0 }}>
-                                    Tail:
-                                </Typography.Paragraph>
-                                <Tooltip title={'Options: all, 100, 500, 1000, etc. '}>
-                                    <Input
-                                        title={'tail'}
-                                        size={'small'}
-                                        onChange={handleTailChanged}
-                                        value={linesToShow}
-                                        status={linesError ? 'error' : 'success'}
-                                        style={{ width: '100px', height: '25px' }} />
-                                </Tooltip>
-                            </Flex>
-                        ]}
-                    />}
-                </Flex>
-            </Card>
-        </ClickAwayListener>
+        <Backdrop>
+            <ClickAwayListener onClickAway={() => { }}>
+                <Card className={styles['modal-container']}>
+                    <Flex vertical gap={10}>
+                        {dataIsLoading && <Spin />}
+                        {!dataIsLoading && <CodeBlock
+                            title={data?.names[0] ?? 'Log'}
+                            content={lines.join('\n')}
+                            tools={[
+                                <Flex align={'center'} gap={5}>
+                                    <Typography.Paragraph style={{ marginBottom: 0 }}>
+                                        Tail:
+                                    </Typography.Paragraph>
+                                    <Tooltip title={'Options: all, 100, 500, 1000, etc. '}>
+                                        <Input
+                                            title={'tail'}
+                                            size={'small'}
+                                            onChange={handleTailChanged}
+                                            value={linesToShow}
+                                            status={linesError ? 'error' : 'success'}
+                                            style={{ width: '100px', height: '25px' }} />
+                                    </Tooltip>
+                                </Flex>
+                            ]}
+                        />}
+                    </Flex>
+                </Card>
+            </ClickAwayListener>
+        </Backdrop>
     )
 }
